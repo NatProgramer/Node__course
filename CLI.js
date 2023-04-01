46eff01744f2d8caf5e0ba91900922f0f1faad6e
NatProgramer <rodrigezsantiago40@gmail.com>
2023-03-31 19:54:26 -0500

    second commit second commit for node__course no serius archive

---
+++ CLI.js
@@ -0,0 +1,5 @@
+console.log('Hola mundo = clasico');
+
+console.log('Calculadora personal');
+
+console.log('Recorrer array');
--- http.js
+++ http.js
@@ -1,9 +1,26 @@
 const http = require('http');
 
-http.createServer((request, response) => {
-	console.log(request);
-	response.write('<h1>Hello worl<h1>');
-	response.end();
-}).listen(3000);
+const server = http.createServer((request, response) => {
+	console.log(request.url);
+
+	if (request.url == '/') {
+		response.write('<h1>Bienvenido a mi server</h1>');
+		return response.end();
+	} else if (request.url === '/about') {
+		response.write('<h1>Acerca de:</h1>');
+		return response.end();
+	} else if (request.url === '/about_us') {
+		response.write('<h1>Somos una asociasion</h1>');
+		return response.end();
+	} else {
+		response.write(`
+		<h1>No se encontro el servidor</h1>
+		<a href="/">Volver a la pagina principal</a>
+		`);
+		return response.end();
+	}
+});
+
+server.listen(3000);
 
 console.log('server escucha el puerto 3000');
